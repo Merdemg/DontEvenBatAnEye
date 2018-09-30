4eeb39080e991613ae73e99cea419edd60947c75
Erdem Gunay <merdemgunay88@gmail.com>
2018-09-29 20:11:42 -0400

    container scripts

--- Assets/Container.cs
+++ Assets/Container.cs
@@ -2,20 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Stairs : MonoBehaviour {
-    [SerializeField] float useTime = 2.0f;
+public class Container : MonoBehaviour {
+
+    bool hasBooze = false;
+    bool hasEvidence = false;
+    [SerializeField] float useTime = 4.0f;
+
+
+    GameObject player;
     [SerializeField] GameObject feedbackObj;
     [SerializeField] float interactDistance = 1.0f;
-
-    [SerializeField] GameObject myPair;
-
-    bool feedbackOn = false;
-    GameObject player;
-     bool playerCanInteract = false;
-     bool isPlayerInteracting = false;
-
+    bool playerCanInteract = false;
     float timer = 0;
-
+    bool isPlayerInteracting = false;
     // Use this for initialization
     void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -27,9 +26,10 @@ public class Stairs : MonoBehaviour {
         if (Vector3.Distance(this.transform.position, player.transform.position) <= interactDistance)
         {
             feedbackObj.GetComponent<SpriteRenderer>().enabled = true;
+
             player.GetComponent<PlayerControl>().setObject2Interact(this.gameObject);
             playerCanInteract = true;
-            // Programmers of the future, if you're reading this, I'm terribly sorry for these scripts
+
         }
         else
         {
@@ -39,7 +39,6 @@ public class Stairs : MonoBehaviour {
         }
 
 
-
         if (isPlayerInteracting)
         {
             timer += Time.deltaTime;
@@ -47,11 +46,20 @@ public class Stairs : MonoBehaviour {
             if (timer >= useTime)
             {
                 timer = 0;
-                player.transform.position = myPair.transform.position;
+                // do stuff
+                if (hasEvidence)
+                {
+                    player.GetComponent<PlayerControl>().getEvidence();
+                }
+                if (hasBooze)
+                {
+                    player.GetComponent<PlayerControl>().getBooze();
+                }
+
+                Destroy(this);
             }
         }
-	}
-
+    }
 
     public void getInteracted()
     {
@@ -62,14 +70,22 @@ public class Stairs : MonoBehaviour {
         }
 
         timer = 0;
-
-
     }
-
 
     public void stopBeingInteracted()
     {
         timer = 0;
         isPlayerInteracting = false;
+    }
+
+
+    public void getEvidence()
+    {
+        hasEvidence = true;
+    }
+
+    public void getBooze()
+    {
+        hasBooze = true;
     }
 }
---
+++ Assets/ContainerProcedure.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContainerProcedure : MonoBehaviour {
+    GameObject[] containers;
+    [SerializeField] int boozeNum = 5;
+    [SerializeField] int evidenceNum = 6;
+
+    // Use this for initialization
+    void Start () {
+        containers = GameObject.FindGameObjectsWithTag("Container");
+
+
+
+
+
+        for (int i = 0; i < 100; i++)
+        {
+            int a = Random.Range(0, containers.Length);
+            int b;
+            do
+            {
+                b = Random.Range(0, containers.Length);
+            } while (a == b);
+
+            GameObject temp = containers[a];
+            containers[a] = containers[b];
+            containers[b] = temp;
+        }
+
+
+        for (int i = 0; i < boozeNum; i++)
+        {
+            containers[i].GetComponent<Container>().getBooze();
+        }
+
+        for (int i = 0; i < 100; i++)
+        {
+            int a = Random.Range(0, containers.Length);
+            int b;
+            do
+            {
+                b = Random.Range(0, containers.Length);
+            } while (a == b);
+
+            GameObject temp = containers[a];
+            containers[a] = containers[b];
+            containers[b] = temp;
+        }
+
+        for (int i = 0; i < boozeNum; i++)
+        {
+            containers[i].GetComponent<Container>().getEvidence();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
