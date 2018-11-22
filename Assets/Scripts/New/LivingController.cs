c21bd6b82620386de1d42a448443960590102d87
Naveen Prasad <36249058+nprsd@users.noreply.github.com>
2018-11-21 19:16:19 -0500

    Added seperate classes to Player and added documentation

--- Assets/Scripts/New/LivingController.cs
+++ Assets/Scripts/New/LivingController.cs
@@ -6,41 +6,17 @@ using Rewired;
 
 public class LivingController : MonoBehaviour {
 
-    [Header("Layer Mask")]
-    [SerializeField] LayerMask myMask;
-    [Header("Float Variables")]
-    [SerializeField] float moveSpeed = 1f;
+
     [SerializeField] float sanity = 100.0f;
-    [SerializeField] float drinkingSpeed = 1.0f;
-    [SerializeField] float boozeSanity = 33.3f;
-    [SerializeField] float mineCost = 25f;
-    [SerializeField] float mineDropTime = 2.0f;
-    [SerializeField] float wardCost = 25f;
-    [SerializeField] float wardDropTime = 1.0f;
-    [SerializeField] float protectionValue = 0.25f;
-    [Header("Images")]
-    [SerializeField] Image BoozeProgress;
-    [SerializeField] Image WardProgress;
-    [SerializeField] Image TrapProg;
-    [SerializeField] Image iconBooze, iconTrap, iconWard, buttonBooze, buttonTrap, buttonWard;
     public Image SanUI;
     [Header("UI Text")]
     [SerializeField] Text evidenceText;
     [SerializeField] Text sanityText, boozeText;
-    [Header("Game Actors")]
-    [SerializeField] GameObject mine;
-    [SerializeField] GameObject ward;
-    [Header("Variable Objects")]
-    public GameObject[] wards;
     public GameObject object2interact;
+    public float protectionValue = 0.25f;
+    public static bool isProtected = false;
 
-    public Transform offset;
-    [Header("Bools")]
-    bool isDrinking = false;
-    bool isDroppingWard = false;
     bool isInteracting = false;
-    bool isDroppingMine = false;
-    bool isProtected = false;
     public static bool isLit = false;
 
     float timer = 0;
@@ -55,193 +31,23 @@ public class LivingController : MonoBehaviour {
     int evidenceRequired = 5;
     int boozeNum = 1;
     int indexWard = 0;
-
-    [Header("Rewiered Player Index")]
     private Player player;
-    public int playerId = 0;
-
-    private void Awake()
-    {
-        player = ReInput.players.GetPlayer(playerId);
-    }
+    [SerializeField] LayerMask myMask;
 
     void Start ()
     {
         updateSanityUI();
+        int playerIdentity = Movement.playerId;
+        player = ReInput.players.GetPlayer(playerIdentity);
     }
 	
 	void Update ()
     {
 
-        RaycastHit2D hit = Physics2D.Raycast(offset.position, -Vector2.up);
-
         if (FindObjectOfType<ward>())
         {
-            isProtected = CheckProtected();
+           isProtected = CheckProtected();
         }
-
-        if (boozeNum > 0)
-        {
-            buttonBooze.color = new Color(buttonBooze.color.r, buttonBooze.color.g, buttonBooze.color.b, 1.0f);
-            iconBooze.color = new Color(iconBooze.color.r, iconBooze.color.g, iconBooze.color.b, 1.0f);
-        }
-        else
-        {
-            buttonBooze.color = new Color(buttonBooze.color.r, buttonBooze.color.g, buttonBooze.color.b, 0.2f);
-            iconBooze.color = new Color(iconBooze.color.r, iconBooze.color.g, iconBooze.color.b, 0.2f);
-
-        }
-
-        if (sanity > wardCost)
-        {
-            buttonWard.color = new Color(buttonWard.color.r, buttonWard.color.g, buttonWard.color.b, 1.0f);
-            iconWard.color = new Color(iconWard.color.r, iconWard.color.g, iconWard.color.b, 1.0f);
-        }
-        else
-        {
-            buttonWard.color = new Color(buttonWard.color.r, buttonWard.color.g, buttonWard.color.b, 0.2f);
-            iconWard.color = new Color(iconWard.color.r, iconWard.color.g, iconWard.color.b,0.2f);
-        }
-
-        if (sanity > mineCost)
-        {
-            buttonTrap.color = new Color(buttonTrap.color.r, buttonTrap.color.g, buttonTrap.color.b, 1.0f);
-            iconTrap.color = new Color(iconTrap.color.r, iconTrap.color.g, iconTrap.color.b, 1.0f);
-        }
-        else
-        {
-            buttonTrap.color = new Color(buttonTrap.color.r, buttonTrap.color.g, buttonTrap.color.b, 0.2f);
-            iconTrap.color = new Color(iconTrap.color.r, iconTrap.color.g, iconTrap.color.b, 0.2f);
-        }
-
-        if(blinkTimer > 0)
-        {
-            blinkTimer -= Time.deltaTime;
-            blinkSwitchTimer += Time.deltaTime;
-
-            if (blinkSwitchTimer >= blinkSpeed)
-            {
-                blinkSwitchTimer -= blinkSpeed;
-                GetComponent<SpriteRenderer>().enabled = !GetComponent<SpriteRenderer>().enabled;
-            }
-            if (sanity >= (.6 * maxSanity))
-            {
-                GetComponent<SpriteRenderer>().color = Color.green;
-                SanUI.color = Color.green;
-            }
-            else if (sanity >= (.3 * maxSanity))
-            {
-                GetComponent<SpriteRenderer>().color = Color.yellow;
-                SanUI.color = Color.yellow;
-            }
-            else
-            {
-                GetComponent<SpriteRenderer>().color = Color.red;
-                SanUI.color = Color.red;
-            }
-        }
-        else
-        {
-            GetComponent<SpriteRenderer>().enabled = true;
-
-        }
-
-
-        if (player.GetButtonDown("Interact") && isDroppingMine == false && isDroppingWard == false && isDrinking == false)
-        {
-            isInteracting = true;
-            interact();
-        }
-        else
-        {
-            //print("Not Interactable");
-        }
-        if (player.GetButtonUp("Interact"))
-        {
-            isInteracting = false;
-            uninteract();
-        }
-
-        if (player.GetButtonDown("Mine") && isInteracting == false && sanity > mineCost && isDroppingWard == false && isDrinking == false)
-        {
-            isDroppingMine = true;
-            timer = 0;
-        }
-        if (player.GetButtonUp("Mine"))
-        {
-            isDroppingMine = false;
-            TrapProg.fillAmount = 0;
-        }
-
-        if (isDroppingMine)
-        {
-            timer += Time.deltaTime;
-            TrapProg.fillAmount = timer / mineDropTime;
-            if (timer >= mineDropTime)
-            {
-                isDroppingMine = false;
-                Instantiate(mine, this.transform.position, this.transform.rotation);
-                drainSanity(mineCost);
-                TrapProg.fillAmount = 0;
-            }
-        }
-
-        if (player.GetButtonDown("Ward") && isInteracting == false && sanity > wardCost && isDroppingMine == false && isDrinking == false)
-        {
-            isDroppingWard = true;
-            timer = 0;
-        }
-        if (player.GetButtonUp("Ward"))
-        {
-            isDroppingWard = false;
-            WardProgress.fillAmount = 0;
-        }
-
-        if (isDroppingWard)
-        {
-            timer += Time.deltaTime;
-            WardProgress.fillAmount = timer / wardDropTime;
-            if (timer >= wardDropTime)
-            {
-                isDroppingWard = false;
-                Instantiate(ward, this.transform.position, this.transform.rotation);
-                drainSanity(wardCost);
-                WardProgress.fillAmount = 0;
-            }
-        }
-
-
-        if (player.GetButtonDown("Booze") && isInteracting == false && isDroppingMine == false && isDroppingWard == false && boozeNum>0)
-        {
-            isDrinking = true;
-            timer = 0;
-        }
-
-        if (isDrinking)
-        {
-            timer += Time.deltaTime;
-            BoozeProgress.fillAmount = timer / drinkingSpeed;
-            if (timer >= drinkingSpeed)
-            {
-                isDrinking = false;
-                boozeNum--;
-                gainSanity(boozeSanity);
-                BoozeProgress.fillAmount = 0;
-            }
-        }
-
-        if (isInteracting == false && isDroppingMine == false && isDroppingWard == false && 
-            (player.GetAxis("Horizontal") > 0.1f || player.GetAxis("Horizontal") < -0.1f
-             || player.GetAxis("Vertical") > 0.1f || player.GetAxis("Vertical") < -0.1f))
-        {          
-            float angle = Mathf.Atan2(player.GetAxis("Horizontal"), -player.GetAxis("Vertical")) * Mathf.Rad2Deg;
-            this.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-            Vector2 temp = new Vector2(player.GetAxis("Horizontal"), player.GetAxis("Vertical"));
-            temp.Normalize();
-            temp *= moveSpeed;
-            GetComponent<Rigidbody2D>().AddForce(temp);
-        }
-
         //Highlight Interactable Objects
         if (player.GetButton("Highlight"))
         {
@@ -249,31 +55,16 @@ public class LivingController : MonoBehaviour {
         }
         else
             isLit = false;
-        
-        
+
     }
 
-    bool CheckProtected()
-    {
-        ward[] wards = FindObjectsOfType<ward>();
-        foreach (ward ward in wards)
-        {
-            RaycastHit2D temp = Physics2D.Raycast(ward.transform.position, this.transform.position - ward.transform.position, myMask);
-            if (temp.transform.gameObject.GetComponent<LivingController>())
-            {
-                return true;
-            }
-
-        }
-        return false;
-    }
 
     public void setObject2Interact(GameObject obj)
     {
         object2interact = obj;
     }
 
-    void interact()
+    public void interact()
     {
 
         if (object2interact)
@@ -298,7 +89,7 @@ public class LivingController : MonoBehaviour {
     }
 
 
-    void uninteract()
+    public void uninteract()
     {
         if (object2interact)
         {
@@ -329,6 +120,7 @@ public class LivingController : MonoBehaviour {
 
     public void drainSanity(float amount)
     {
+        
         if (isProtected)
             amount *= protectionValue;
 
@@ -344,7 +136,7 @@ public class LivingController : MonoBehaviour {
         blinkTimer = blinkTimerMax;
     }
 
-    void gainSanity(float amount)
+    public void gainSanity(float amount)
     {
         sanity += amount;
 
@@ -378,6 +170,20 @@ public class LivingController : MonoBehaviour {
     {
         boozeNum++;
         updateSanityUI();
+    }
+    bool CheckProtected()
+    {
+        ward[] wards = FindObjectsOfType<ward>();
+        foreach (ward ward in wards)
+        {
+            RaycastHit2D temp = Physics2D.Raycast(ward.transform.position, this.transform.position - ward.transform.position, myMask);
+            if (temp.transform.gameObject.GetComponent<LivingController>())
+            {
+                return true;
+            }
+
+        }
+        return false;
     }
 
 }
---
+++ Assets/Scripts/New/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+public class Movement : MonoBehaviour {
+
+    [Header("Rewired Player Index")]
+    private Player player;
+    public static int playerId = 0;
+
+    [SerializeField] float moveSpeed = 1f;
+
+    private void Awake()
+    {
+        player = ReInput.players.GetPlayer(playerId);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if ((player.GetAxis("Horizontal") > 0.1f || player.GetAxis("Horizontal") < -0.1f
+             || player.GetAxis("Vertical") > 0.1f || player.GetAxis("Vertical") < -0.1f))
+        {
+            float angle = Mathf.Atan2(player.GetAxis("Horizontal"), -player.GetAxis("Vertical")) * Mathf.Rad2Deg;
+            this.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            Vector2 temp = new Vector2(player.GetAxis("Horizontal"), player.GetAxis("Vertical"));
+            temp.Normalize();
+            temp *= moveSpeed;
+            GetComponent<Rigidbody2D>().AddForce(temp);
+        }
+
+    }
+}
