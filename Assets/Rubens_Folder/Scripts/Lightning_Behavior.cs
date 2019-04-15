b9b3bc33470a3b130a8b34d85354452928494bf7
Naveen Prasad <36249058+nprsd@users.noreply.github.com>
2019-04-15 01:15:03 -0400

    Before Erdem's scene

---
+++ Assets/Rubens_Folder/Scripts/Lightning_Behavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightning_Behavior : MonoBehaviour
+{
+    [SerializeField] Animator anim_Light;
+    [SerializeField] float speed;
+    [SerializeField] private float timer;
+    //[SerializeField] GameObject gmObj_lighting;
+
+    private void Awake()
+    {
+        
+        timer = Random.Range(50f, 550f);
+    }
+    private void FixedUpdate()
+    {
+        transform.Rotate(0,1,0,Space.World);
+        timer -= 1 / (Time.deltaTime /speed);
+        if (timer < 0) {
+            print("Thunder");
+            anim_Light.SetTrigger("Thunder");
+            ResetTimer();
+        }
+    }
+
+
+    void ResetTimer()
+    {
+        timer = Random.Range(25f, 150f);
+        
+    }
+}
